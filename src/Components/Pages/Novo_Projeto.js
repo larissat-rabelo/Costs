eac8f39da545fc418b61b40de0aeb2881f0e8c15
larissat.rabelo <larissateixeirarabelo@gmail.com>
2023-04-11 15:17:31 -0300

    Adicionando mensagem de erro e sucesso ao criar um novo orçamento

--- src/Components/Pages/Novo_Projeto.js
+++ src/Components/Pages/Novo_Projeto.js
@@ -1,12 +1,37 @@
+import { useNavigate } from 'react-router-dom'
 import Form_Proj from '../Projetos/Form_Proj'
 import styles from './Novo_Projeto.module.css'
 
-function Novo_Projeto({btnText}) {
+function Novo_Projeto() {
+    const navigate = useNavigate()
+
+    function createPost(project) {
+
+        project.cost = 0
+        project.services = []
+
+        fetch("http://localhost:5000/projects", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(project)
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            
+
+            navigate.push('/projects',{message: 'Projeto criado com sucesso!'})
+        })
+        .catch((err) => console.log(err)) 
+    }
+
     return (
         <div className={styles.novoproj_container}>
              <h1>Criar projeto</h1>
              <p>Crie seu projeto para depois adicionar os serviços</p>
-             <Form_Proj btnText="Criar projeto"/>
+             <Form_Proj handleSubmit={createPost} btnText="Criar projeto"/>
         </div>
        
     )
--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -1,6 +1,17 @@
+import {useLocation} from 'react-router-dom'
+import Message from "../Layout/Message"
+
 function Projects() {
+    const location = useLocation()
+    let message = ''
+    if(location.state) {
+        message = location.state.message
+    }
     return (
-        <h1>Projects</h1>
+       <div>
+        <h1>Meus projetos</h1>
+        {message && <Message type ="sucess" msg={message}/>}
+       </div>
     )
 }
 
